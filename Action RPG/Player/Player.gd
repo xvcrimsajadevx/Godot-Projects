c978edc6d1f40dd4787c56fd8402d64a3c29c5d9
xvcrimsajadevx <xvcrimsajadevx@hotmail.com>
2022-12-02 00:34:21 -0700

    Make Action RPG: Initial Commit

---
+++ Action RPG/Player/Player.gd
@@ -0,0 +1,25 @@
+extends KinematicBody2D
+
+var velocity = Vector2.ZERO
+
+export var ACCELERATION = 400 # Make cost when finalized
+export var MAX_SPEED = 100 # Make cost when finalized
+export var FRICTION = 400 # Make cost when finalized
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+	
+	if input_vector != Vector2.ZERO:
+		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+	
+	velocity = move_and_slide(velocity)
