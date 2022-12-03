390fd703cc9843237109f8b6feaa55e702832206
xvcrimsajadevx <xvcrimsajadevx@hotmail.com>
2022-12-03 02:37:14 -0700

    ActionRPG - Player Movement/Animation State Machine

--- Action RPG/Player/Player.gd
+++ Action RPG/Player/Player.gd
@@ -1,10 +1,14 @@
 extends KinematicBody2D
 
+export var ACCELERATION = 500 # Make cost when finalized
+export var MAX_SPEED = 80 # Make cost when finalized
+export var FRICTION = 500 # Make cost when finalized
+
 var velocity = Vector2.ZERO
 
-export var ACCELERATION = 400 # Make cost when finalized
-export var MAX_SPEED = 100 # Make cost when finalized
-export var FRICTION = 400 # Make cost when finalized
+onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -18,8 +22,12 @@ func _physics_process(delta):
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
+		animationTree.set("parameters/Idle/blend_position", input_vector)
+		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationState.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
+		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
 	velocity = move_and_slide(velocity)
