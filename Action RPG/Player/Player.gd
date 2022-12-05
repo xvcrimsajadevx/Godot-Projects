6919e264b22f5a51401f300474ccb3d2a091123d
xvcrimsajadevx <xvcrimsajadevx@hotmail.com>
2022-12-05 13:31:02 -0700

    Action RPG - Roll State

--- Action RPG/Player/Player.gd
+++ Action RPG/Player/Player.gd
@@ -2,6 +2,7 @@ extends KinematicBody2D
 
 export var ACCELERATION = 500 # Make cost when finalized
 export var MAX_SPEED = 80 # Make cost when finalized
+export var ROLL_SPEED = 125 # Make cost when finalized
 export var FRICTION = 500 # Make cost when finalized
 
 enum {
@@ -12,6 +13,7 @@ enum {
 
 var state = MOVE
 var velocity = Vector2.ZERO
+var roll_vector = Vector2.RIGHT
 
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
@@ -38,22 +40,33 @@ func move_state(delta):
 	input_vector = input_vector.normalized()
 	
 	if input_vector != Vector2.ZERO:
+		roll_vector = input_vector
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
 		animationTree.set("parameters/Attack/blend_position", input_vector)
+		animationTree.set("parameters/Roll/blend_position", input_vector)
 		animationState.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
 		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 	
-	velocity = move_and_slide(velocity)
+	move()
+	
+	if Input.is_action_just_pressed("roll"):
+		state = ROLL
 	
 	if Input.is_action_just_pressed("attack"):
 		state = ATTACK
 
 func roll_state(delta):
-	pass
+	velocity = roll_vector * ROLL_SPEED
+	animationState.travel("Roll")
+	move()
+
+func roll_animation_finished():
+	velocity = velocity / 3
+	state = MOVE
 
 func attack_state(delta):
 	velocity = Vector2.ZERO
@@ -61,3 +74,6 @@ func attack_state(delta):
 
 func attack_animation_finished():
 	state = MOVE
+
+func move():
+	velocity = move_and_slide(velocity)
