07df4d03546c32415b3ff550cbd9e94e29388dc0
xvcrimsajadevx <xvcrimsajadevx@hotmail.com>
2022-12-05 16:22:37 -0700

    Action RPG - Knockback and Enemy Bat

--- Action RPG/Player/Player.gd
+++ Action RPG/Player/Player.gd
@@ -18,10 +18,12 @@ var roll_vector = Vector2.RIGHT
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
+onready var swordHitbox = $HitboxPivot/SwordHitbox
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	animationTree.active = true
+	swordHitbox.knockback_vector = roll_vector
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
@@ -41,6 +43,7 @@ func move_state(delta):
 	
 	if input_vector != Vector2.ZERO:
 		roll_vector = input_vector
+		swordHitbox.knockback_vector = input_vector
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
 		animationTree.set("parameters/Attack/blend_position", input_vector)
