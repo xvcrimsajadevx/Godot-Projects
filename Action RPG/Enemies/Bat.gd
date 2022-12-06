d5a58284765c095ed363bccef48ec342da909472
xvcrimsajadevx <xvcrimsajadevx@hotmail.com>
2022-12-06 02:44:55 -0700

    Action RPG - Bat AI start

--- Action RPG/Enemies/Bat.gd
+++ Action RPG/Enemies/Bat.gd
@@ -2,13 +2,49 @@ extends KinematicBody2D
 
 const EnemyDeathEffect = preload("res://Effects/EnemyDeathEffect.tscn")
 
+export var ACCELERATION = 300
+export var MAX_SPEED = 50
+export var FRICTION = 200
+
+var state = CHASE
+enum {
+	IDLE,
+	WANDER,
+	CHASE
+}
+
+var velocity = Vector2.ZERO
 var knockback = Vector2.ZERO
 
+onready var sprite = $AnimatedSprite
 onready var stats = $Stats
+onready var playerDetectionZone = $PlayerDetectionZone
 
 func _physics_process(delta):
-	knockback = knockback.move_toward(Vector2.ZERO, 200 * delta)
+	knockback = knockback.move_toward(Vector2.ZERO, FRICTION * delta)
 	knockback = move_and_slide(knockback)
+	
+	match state:
+		IDLE:
+			velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+			seek_player()
+		
+		WANDER:
+			pass
+		
+		CHASE:
+			var player = playerDetectionZone.player
+			if player != null:
+				var direction = (player.global_position - global_position).normalized()
+				velocity = velocity.move_toward(direction * MAX_SPEED, ACCELERATION * delta)
+			else:
+				state = IDLE
+			sprite.flip_h = velocity.x < 0
+	velocity = move_and_slide(velocity)
+
+func seek_player():
+	if playerDetectionZone.can_see_player():
+		state = CHASE
 
 func _on_HurtBox_area_entered(area):
 	stats.health -= area.damage
