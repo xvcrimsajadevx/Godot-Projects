ff24140950cbb358d7d3155b27e10283de1a6346
xvcrimsajadevx <xvcrimsajadevx@hotmail.com>
2022-12-08 18:53:06 -0700

    Enemy Soft Collisions + Profiling

--- Action RPG/Enemies/Bat.gd
+++ Action RPG/Enemies/Bat.gd
@@ -20,6 +20,7 @@ onready var sprite = $AnimatedSprite
 onready var stats = $Stats
 onready var playerDetectionZone = $PlayerDetectionZone
 onready var hurtbox = $Hurtbox
+onready var softCollision = $SoftCollision
 
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, FRICTION * delta)
@@ -41,6 +42,9 @@ func _physics_process(delta):
 			else:
 				state = IDLE
 			sprite.flip_h = velocity.x < 0
+	
+	if softCollision.is_colliding():
+		velocity += softCollision.get_push_vector() * delta * 400
 	velocity = move_and_slide(velocity)
 
 func seek_player():
