1d6c3add909ff6dee6b8b2dcf25326faab258881
xvcrimsajadevx <xvcrimsajadevx@hotmail.com>
2022-12-15 01:36:06 -0700

    1-Bit Course: Piggy - Complete

---
+++ 1-Bit Course - 2) Piggy/Piggy Game Resources/Pig.gd
@@ -0,0 +1,34 @@
+extends Area2D
+
+export(int) var SPEED = 50
+var moving = false
+
+onready var animationPlayer = $AnimationPlayer
+onready var sprite = $Sprite
+
+func _process(delta):
+	moving = false
+	if Input.is_action_pressed("ui_right"):
+		move(SPEED, 0, delta)
+		sprite.flip_h = false
+	if Input.is_action_pressed("ui_left"):
+		move(-SPEED, 0, delta)
+		sprite.flip_h = true
+	if Input.is_action_pressed("ui_up"):
+		move(0, -SPEED, delta)
+	if Input.is_action_pressed("ui_down"):
+		move(0, SPEED, delta)
+		
+	if moving == true:
+		animationPlayer.play("Run")
+	else:
+		animationPlayer.play("Idle")
+
+func move(xspeed, yspeed, delta):
+	position.x += xspeed * delta
+	position.y += yspeed * delta
+	moving = true
+
+func _on_Pig_area_entered(area):
+	area.queue_free()
+	scale *= 1.1
