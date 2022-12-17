37aec54793b87feba9a8bd19dc2c7ad6d87a5ce0
xvcrimsajadevx <xvcrimsajadevx@hotmail.com>
2022-12-16 17:50:57 -0700

    1-Bit Course: Space Shooter - Label/Setters and Getters (Player Score)

--- 1-Bit Course - 3) Space Shooter/Resources/Ship.gd
+++ 1-Bit Course - 3) Space Shooter/Resources/Ship.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 const Bullet = preload("res://Resources/Bullet.tscn")
+const ExplosionEffect = preload("res://Resources/ExplosionEffect.tscn")
 
 export(int) var SPEED = 100
 
@@ -22,3 +23,9 @@ func fire_bullet():
 func _on_Ship_area_entered(area):
 	area.queue_free()
 	queue_free()
+
+func _exit_tree():
+	var main = get_tree().current_scene
+	var explosionEffect = ExplosionEffect.instance()
+	main.call_deferred("add_child", explosionEffect)
+	explosionEffect.global_position = global_position
