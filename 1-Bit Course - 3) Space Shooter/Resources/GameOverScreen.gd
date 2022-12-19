66fd4c658313679c09c77cb02ff26994d1075d12
xvcrimsajadevx <xvcrimsajadevx@hotmail.com>
2022-12-18 21:25:13 -0700

    1-Bit Course: Space Shooter - JSON Save and Load

--- 1-Bit Course - 3) Space Shooter/Resources/GameOverScreen.gd
+++ 1-Bit Course - 3) Space Shooter/Resources/GameOverScreen.gd
@@ -1,6 +1,15 @@
 extends Node
 
+onready var highscoreLabel = $HighscoreLabel
+
+func _ready():
+	set_highscore_label()
+
 func _process(_delta):
 	if Input.is_action_just_pressed("ui_cancel"):
-# warning-ignore:return_value_discarded
+	# warning-ignore:return_value_discarded
 		get_tree().change_scene("res://Resources/StartMenu.tscn")
+
+func set_highscore_label():
+	var save_data = SaveAndLoad.load_data_from_file()
+	highscoreLabel.text = "Highscore: " + str(save_data.highscore)
