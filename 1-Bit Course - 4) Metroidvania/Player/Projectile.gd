304e6970994f795b5ec785c7a9685c7c8a94c30d
xvcrimsajadevx <xvcrimsajadevx@hotmail.com>
2022-12-23 19:40:15 -0700

    1-Bit Course: Metroidvania - Explosion and Jump Effects

--- 1-Bit Course - 4) Metroidvania/Player/Projectile.gd
+++ 1-Bit Course - 4) Metroidvania/Player/Projectile.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+const ExplosionEffect = preload("res://Effects/ExplosionEffect.tscn")
+
 var velocity = Vector2.ZERO
 
 func _process(delta):
@@ -9,8 +11,10 @@ func _process(delta):
 func _on_VisibilityNotifier2D_viewport_exited(viewport):
 	queue_free()
 
-func _on_Hitbox_body_entered(body):
+func _on_Hitbox_body_entered(_body):
+	Utils.instance_scene_on_main(ExplosionEffect, global_position)
 	queue_free()
 
-func _on_Hitbox_area_entered(area):
+func _on_Hitbox_area_entered(_area):
+	Utils.instance_scene_on_main(ExplosionEffect, global_position)
 	queue_free()
